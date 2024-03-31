0756b5099d4ac16a82d173f095f23d4f298611f9
Enzo Manuel Mangano <enzomanuelmangano@gmail.com>
2024-03-31 10:57:30 +0200

    docs: document types

--- src/types.ts
+++ src/types.ts
@@ -1,19 +1,50 @@
 import type { StyleProp, ViewStyle } from 'react-native';
 import type { ErrorCorrectionLevelType } from './qrcode/generate-matrix';
 
+/**
+ * Represents a base shared value.
+ * @template T - The type of the shared value.
+ */
 export type BaseSharedValue<T> = {
   value: T;
 };
 
+/**
+ * Represents the props for a QRCode component.
+ */
 export type QRCodeProps = {
+  /** The value encoded in the QRCode. */
   value: string;
+  /** The style applied to the QRCode container. */
   style?: StyleProp<ViewStyle>;
-  // Level L 7%, level M 15%, level Q 25%, level H 30%.
+  /**
+   * The error correction level for the QRCode.
+   * Level L: 7%, level M: 15%, level Q: 25%, level H: 30%.
+   * Default value is 'H'.
+   */
   errorCorrectionLevel?: ErrorCorrectionLevelType;
+  /** The color of the QRCode path.
+   * Default value is '#000000'.
+   */
   pathColor?: string;
+  /**
+   * The percentage of the strokeWidth (0 to 1)
+   * Can be a number or a BaseSharedValue<number>.
+   * Default value is 1.
+   */
   strokeWidthPercentage?: number | BaseSharedValue<number>;
+  /** The children components rendered within the QRCode container. */
   children?: React.ReactNode;
+  /**
+   * The style of the QRCode path: 'fill' or 'stroke'.
+   * Default value is 'stroke'.
+   */
   pathStyle?: 'fill' | 'stroke';
+  /**
+   * The padding applied around the QRCode.
+   * Default value is 0.
+   */
   padding?: number;
+  /** The size of the QRCode. */
   size: number;
 };
